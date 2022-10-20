8e225395f1ff159bbdfe24c865246ad99b6cf461
Ann-Yurchuk <105579383+Ann-Yurchuk@users.noreply.github.com>
2022-10-20 19:10:44 +0300

    add task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import 'lazysizes';
+
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryImage = onClickGalleryContainer(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryImage);
+
+function onClickGalleryContainer(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image lazyload"
+        data-src="${preview}"
+        alt="${description}" />
+      </a>`;
+    }).join('');
+};
+
+   const lightbox = new SimpleLightbox('.gallery a', {
+    disableRightClick: true,
+    enableKeyboard: true,
+    scrollZoom: false,
+    captionDelay: 250,
+    captionsData: 'alt',
+});
